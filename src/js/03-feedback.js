5820f23fe18556a4a3ade5e9cb5f030e5635cff0
Max3281 <113448092+Max3281@users.noreply.github.com>
2023-02-18 17:12:44 +0300

    edit 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,6 @@
 import throttle from 'lodash.throttle';
 
 const formMarkEl = document.querySelector('.feedback-form');
-// const inputMarkEl = document.querySelector('input');
-// const textMarkEl = document.querySelector('textarea');
 
 let formData = {};
 const STORAGE_KEY = 'feedback-form-state';
@@ -10,38 +8,25 @@ const STORAGE_KEY = 'feedback-form-state';
 loadFromStorage();
 
 formMarkEl.addEventListener('input', throttle(saveOnStorage, 1000));
-// formMarkEl.addEventListener('submit', onButtonMark);
 
 function saveOnStorage(evt) {
   formData[evt.target.name] = evt.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  // return formData;
 }
-
-// function onInputMark(evt) {
-//   saveOnStorage(evt);
-//   //   console.log(evt.target.value);
-// }
-
-// function onTextMark(evt) {
-//   saveOnStorage(evt);
-//   //   console.log(formData);
-// }
 
 function loadFromStorage() {
   try {
     let checkStorage = localStorage.getItem(STORAGE_KEY);
     if (checkStorage) {
-      formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-      Object.entries(formData).forEach(([name, value]) => {
-        formMarkEl[name] = value;
-        // console.log((formMarkEl[name] = value));
-      });
+      formData = JSON.parse(checkStorage);
       console.log(formData);
+      Object.entries(formData).forEach(([name, value]) => {
+        formMarkEl[name].value = value;
+        console.log(formMarkEl[name]);
+      });
     }
   } catch (error) {
-    console.log(error.name);
-    console.log(error.message);
+    console.log(error.name, ': ', error.message);
   }
 }
 
